9157f686795f94d266db602d0e64d16ee9808e63
Unknown <Klaus.Pruenster@gmail.com>
2019-12-03 19:50:26 +0100

    day 3 star 2

--- Solver/Algorithms/Day3Solver.cs
+++ Solver/Algorithms/Day3Solver.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using Solver.Base;
@@ -7,6 +8,45 @@ using Solver.Model;
 
 namespace Solver.Algorithms
 {
+	public class WirePosition : IEqualityComparer<WirePosition>, IEquatable<WirePosition>
+	{
+		public Point Point { get; set; }
+		public int Distance { get; set; }
+
+		public bool Equals(WirePosition x, WirePosition y)
+		{
+			return GetHashCode(x) == GetHashCode(y);
+		}
+
+		public int GetHashCode(WirePosition obj)
+		{
+			return Point.GetHashCode();
+		}
+
+		public bool Equals(WirePosition other)
+		{
+			if (ReferenceEquals(null, other)) return false;
+			if (ReferenceEquals(this, other)) return true;
+			return Point.Equals(other.Point);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != GetType()) return false;
+			return Equals((WirePosition)obj);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return Point.GetHashCode() * 397;
+			}
+		}
+	}
+
 	public class Day3Solver : ISolver<int, Day3Input>
 	{
 		public int Star1(Day3Input input)
@@ -16,7 +56,6 @@ namespace Solver.Algorithms
 			var currentPos = origin;
 
 			foreach (var i in input.Line1)
-			{
 				for (var j = 0; j < i.Count; j++)
 				{
 					if (i.Direction == 'L')
@@ -30,13 +69,11 @@ namespace Solver.Algorithms
 
 					points1.Add(currentPos);
 				}
-			}
 
 			currentPos = origin;
 			var points2 = new List<Point>();
 
 			foreach (var i in input.Line2)
-			{
 				for (var j = 0; j < i.Count; j++)
 				{
 					if (i.Direction == 'L')
@@ -50,26 +87,102 @@ namespace Solver.Algorithms
 
 					points2.Add(currentPos);
 				}
-			}
 
-			var interceptions = points1.Intersect(points2).ToList();
+			var interceptions = points1.Intersect(points2);
 
 			var manhattanInterceptions = interceptions
-										 .Select(c => new
-													  {
-														  Dist = Math.Abs(c.X - origin.X) + Math.Abs(c.Y - origin.Y),
-														  Point = c
-													  })
-										 .OrderBy(c => c.Dist)
-										 .ToList();
+										 .Select(c => Math.Abs(c.X - origin.X) + Math.Abs(c.Y - origin.Y))
+										 .OrderBy(c => c);
 
 			var closestInterception = manhattanInterceptions.First();
-			return closestInterception.Dist;
+			return closestInterception;
 		}
 
 		public int Star2(Day3Input input)
 		{
-			throw new NotImplementedException();
+			var points1 = new List<WirePosition>();
+			var origin = new Point(input.MaxLeft + input.MaxRight, input.MaxUp + input.MaxDown); // avoid negative values
+			var currentPos = origin;
+			var currentDist = 0;
+			foreach (var i in input.Line1)
+				for (var j = 0; j < i.Count; j++)
+				{
+					if (i.Direction == 'L')
+						currentPos.X -= 1;
+					else if (i.Direction == 'R')
+						currentPos.X += 1;
+					else if (i.Direction == 'U')
+						currentPos.Y -= 1;
+					else if (i.Direction == 'D')
+						currentPos.Y += 1;
+
+					currentDist += 1;
+					points1.Add(new WirePosition
+								{
+									Point = currentPos,
+									Distance = currentDist
+								});
+				}
+
+			currentPos = origin;
+			var points2 = new List<WirePosition>();
+			currentDist = 0;
+
+			foreach (var i in input.Line2)
+				for (var j = 0; j < i.Count; j++)
+				{
+					if (i.Direction == 'L')
+						currentPos.X -= 1;
+					else if (i.Direction == 'R')
+						currentPos.X += 1;
+					else if (i.Direction == 'U')
+						currentPos.Y -= 1;
+					else if (i.Direction == 'D')
+						currentPos.Y += 1;
+
+					currentDist += 1;
+					points2.Add(new WirePosition
+								{
+									Point = currentPos,
+									Distance = currentDist
+								});
+				}
+
+			var i1 = points2.Intersect(points1).OrderBy(c => c.Point.X).ThenBy(c => c.Point.Y).ToList();
+			var i2 = points1.Intersect(points2).OrderBy(c => c.Point.X).ThenBy(c => c.Point.Y).ToList();
+
+			var d1 = i1.GroupBy(c => c.Point)
+					   .Select(c => c.Sum(r => 1))
+					   .OrderByDescending(c => c);
+			var d2 = i2.GroupBy(c => c.Point)
+					   .Select(c => c.Sum(r => 1))
+					   .OrderByDescending(c => c);
+
+			if (d2.First() > 1 || d1.First() > 1)
+			{
+				Console.WriteLine("Duplicated crossings");
+				Debugger.Break();
+			}
+
+			if (i1.Count != i2.Count)
+			{
+				Console.WriteLine("Count is not equal");
+				Debugger.Break();
+			}
+
+			var smallestSum = int.MaxValue;
+			var smallestIndex = -1;
+			for (var i = 0; i < i1.Count; i++)
+			{
+				var dist = i1[i].Distance + i2[i].Distance;
+				if (smallestSum > dist)
+				{
+					smallestSum = dist;
+					smallestIndex = i;
+				}
+			}
+
+			return i1[smallestIndex].Distance + i2[smallestIndex].Distance;
 		}
 	}
 }
--- Test/Day3Test.cs
+++ Test/Day3Test.cs
@@ -45,6 +45,21 @@ namespace Test
 		}
 
 		[Test]
+		[TestCase("R8,U5,L5,D3", "U7,R6,D4,L4", ExpectedResult = 30)]
+		[TestCase("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83", ExpectedResult = 610)]
+		[TestCase("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7", ExpectedResult = 410)]
+		public int Example2(string inp1, string inp2)
+		{
+			var inputStr = new[] { inp1, inp2 };
+			var input = new Day3Input().Parse(inputStr);
+
+			var solution = _solver.Star2(input);
+
+			Console.WriteLine(solution);
+			return solution;
+		}
+
+		[Test]
 		public void Star2()
 		{
 			var fileInput = File.Read();
@@ -53,8 +68,7 @@ namespace Test
 			var solution = _solver.Star2(input);
 
 			Console.WriteLine(solution);
-			Assert.Pass();
-			Assert.AreEqual(7733, solution);
+			Assert.AreEqual(9240, solution);
 		}
 	}
 }
