a0c8997351d212ee6c25427db63425f9776fa741
Unknown <Klaus.Pruenster@gmail.com>
2019-12-08 00:36:31 +0100

    day 7 star 2

--- Solver/Algorithms/Day7Solver.cs
+++ Solver/Algorithms/Day7Solver.cs
@@ -58,7 +58,7 @@ namespace Solver.Algorithms
 					var queue = new Queue<int>();
 					queue.Enqueue(i1);
 					queue.Enqueue(lastOut);
-					lastOut = IntcodeComputer(input, queue);
+					lastOut = IntcodeComputer1((Day7Input)input.Clone(), queue);
 					biggestOut = Math.Max(biggestOut, lastOut);
 				}
 			}
@@ -68,10 +68,63 @@ namespace Solver.Algorithms
 
 		public int Star2(Day7Input input)
 		{
-			throw new NotImplementedException();
+			var biggestOut = 0;
+			var sequence = new List<int> { 5, 6, 7, 8, 9 };
+
+			var permutations = sequence.Permute();
+
+			foreach (var permutation in permutations)
+			{
+				var inpClone = new[]
+							   {
+								   new IntComputer
+								   {
+									   Input = (Day7Input)input.Clone()
+								   },
+								   new IntComputer
+								   {
+									   Input = (Day7Input)input.Clone()
+								   },
+								   new IntComputer
+								   {
+									   Input = (Day7Input)input.Clone()
+								   },
+								   new IntComputer
+								   {
+									   Input = (Day7Input)input.Clone()
+								   },
+								   new IntComputer
+								   {
+									   Input = (Day7Input)input.Clone()
+								   }
+							   };
+
+				var lastOut = 0;
+				while (true)
+				{
+					var final = false;
+					foreach (var phase in permutation)
+					{
+						var queue = new List<int>();
+						queue.Add(phase);
+						queue.Add(lastOut);
+
+						final = inpClone[phase - 5].IntcodeComputer2(queue);
+						lastOut = inpClone[phase - 5].Output;
+					}
+
+					if (final)
+						break;
+				}
+
+
+				biggestOut = Math.Max(biggestOut, lastOut);
+			}
+
+			return biggestOut;
 		}
 
-		public int IntcodeComputer(Day7Input input, Queue<int> inputQueue)
+		public int IntcodeComputer1(Day7Input input, Queue<int> inputQueue)
 		{
 			var cursor = 0;
 			var output = -1;
@@ -177,6 +230,120 @@ namespace Solver.Algorithms
 					pos = input.Commands.Length + pos;
 
 				input.Commands[pos] = val;
+			}
+		}
+	}
+
+	public class IntComputer
+	{
+		public int InputPos { get; set; }
+		public int Cursor { get; set; }
+		public Day7Input Input { get; set; }
+		public int Output { get; set; }
+
+		public bool IntcodeComputer2(List<int> inputQueue)
+		{
+			while (true)
+			{
+				var command = Input.Commands[Cursor];
+
+				var cleanCommand = command;
+				if (command > 10000)
+					throw new Exception("Strange things happened");
+
+				if (command > 100)
+					cleanCommand = int.Parse(command.ToString().Substring(command.ToString().Length - 2));
+
+				if (cleanCommand == 99)
+					return true;
+
+
+				var param = command.ToString().Reverse().Skip(2).ToList();
+				if (param.Count < 1)
+					param.Add('0');
+				if (param.Count < 2)
+					param.Add('0');
+
+				var v1 = param[0] == '0' ? Input.Commands[Input.Commands[Cursor + 1]] : Input.Commands[Cursor + 1];
+
+				if (cleanCommand == 3)
+				{
+					var addr1 = param[0] == '0' ? Input.Commands[Cursor + 1] : Cursor + 1;
+
+					Input.Commands[addr1] = inputQueue[InputPos];
+					InputPos = 1;
+					Cursor += 2;
+					continue;
+				}
+
+				if (cleanCommand == 4)
+				{
+					Output = v1;
+					Cursor += 2;
+					return false;
+				}
+
+				var v2 = param[1] == '0' ? Input.Commands[Input.Commands[Cursor + 2]] : Input.Commands[Cursor + 2];
+				var pos = Input.Commands[Cursor + 3];
+
+				if (pos == Cursor)
+					throw new Exception("Cursor is equal to position");
+
+				int val;
+				if (cleanCommand == 1)
+				{
+					val = v1 + v2;
+					Cursor += 4;
+				}
+				else if (cleanCommand == 2)
+				{
+					val = v1 * v2;
+					Cursor += 4;
+				}
+				// jump-if-true
+				else if (cleanCommand == 5)
+				{
+					if (v1 > 0)
+						Cursor = v2;
+					else
+						Cursor += 3;
+					continue;
+				}
+				// jump-if-false
+				else if (cleanCommand == 6)
+				{
+					if (v1 == 0)
+						Cursor = v2;
+					else
+						Cursor += 3;
+					continue;
+				}
+				// less than
+				else if (cleanCommand == 7)
+				{
+					if (v1 < v2)
+						val = 1;
+					else
+						val = 0;
+					Cursor += 4;
+				}
+				// equals
+				else if (cleanCommand == 8)
+				{
+					if (v1 == v2)
+						val = 1;
+					else
+						val = 0;
+					Cursor += 4;
+				}
+				else
+					throw new Exception("Strange things happened");
+
+
+				if (pos < 0)
+					pos = Input.Commands.Length + pos;
+
+				Input.Commands[pos] = val;
 			}
 		}
 	}
--- Solver/Model/Day7Input.cs
+++ Solver/Model/Day7Input.cs
@@ -1,11 +1,20 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Solver.Base;
 
 namespace Solver.Model
 {
-	public class Day7Input : IInput<Day7Input>
+	public class Day7Input : IInput<Day7Input>, ICloneable
 	{
 		public int[] Commands { get; set; }
+
+		public object Clone()
+		{
+			return new Day7Input
+				   {
+					   Commands = (int[])Commands.Clone()
+				   };
+		}
 
 		public Day7Input Parse(string[] values)
 		{
