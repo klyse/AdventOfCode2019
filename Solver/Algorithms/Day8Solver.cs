26edce8920cbce36020f894288c3436725af5332
Unknown <Klaus.Pruenster@gmail.com>
2019-12-08 00:39:24 +0100

    prepared day 8

---
+++ Solver/Algorithms/Day8Solver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Solver.Base;
+using Solver.Model;
+
+namespace Solver.Algorithms
+{
+	public class Day8Solver : ISolver<int, Day8Input>
+	{
+		public int Star1(Day8Input input)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int Star2(Day8Input input)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Solver/Model/Day8Input.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Solver.Base;
+
+namespace Solver.Model
+{
+	public class Day8Input : IInput<Day8Input>
+	{
+		public Day8Input Parse(string[] values)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
