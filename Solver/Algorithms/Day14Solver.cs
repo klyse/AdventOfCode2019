65dc4b337e5e1eaf4c1eaf443b64727e27efe525
Unknown <Klaus.Pruenster@gmail.com>
2019-12-14 22:30:58 +0100

    day 14 star 1

--- Solver/Algorithms/Day14Solver.cs
+++ Solver/Algorithms/Day14Solver.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Solver.Base;
 using Solver.Model;
 
@@ -6,14 +8,56 @@ namespace Solver.Algorithms
 {
 	public class Day14Solver : ISolver<int, Day14Input>
 	{
+		private Day14Input day14In;
+
+		private int oreCnt;
+		private Dictionary<string, int> ProducedChemicals;
+
 		public int Star1(Day14Input input)
 		{
-			throw new NotImplementedException();
+			day14In = input;
+
+			ProducedChemicals = day14In.Recipes.ToDictionary(c => c.Name, c => 0);
+
+			Make("FUEL");
+
+			return oreCnt;
 		}
 
 		public int Star2(Day14Input input)
 		{
 			throw new NotImplementedException();
+		}
+
+		private void Need(string name, int count)
+		{
+			if (ProducedChemicals[name] >= count)
+				ProducedChemicals[name] -= count;
+			else
+				while (true)
+				{
+					var madeCnt = Make(name);
+
+					ProducedChemicals[name] += madeCnt;
+					if (ProducedChemicals[name] >= count)
+					{
+						ProducedChemicals[name] -= count;
+						break;
+					}
+				}
+		}
+
+		private int Make(string chemName)
+		{
+			var chemical = day14In.Recipes.First(c => c.Name == chemName);
+
+			foreach (var chemicalDependency in chemical.Dependencies)
+				if (chemicalDependency.Name == "ORE")
+					oreCnt += chemicalDependency.Count;
+				else
+					Need(chemicalDependency.Name, chemicalDependency.Count);
+
+			return chemical.ResultCount;
 		}
 	}
 }
--- Solver/Model/Day14Input.cs
+++ Solver/Model/Day14Input.cs
@@ -1,13 +1,55 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Solver.Base;
 
 namespace Solver.Model
 {
+	public class ChemicalDependency
+	{
+		public string Name { get; set; }
+		public int Count { get; set; }
+	}
+
+	public class Chemical
+	{
+		public ICollection<ChemicalDependency> Dependencies { get; set; } = new List<ChemicalDependency>();
+		public int ResultCount { get; set; }
+		public string Name { get; set; }
+	}
+
 	public class Day14Input : IInput<Day14Input>
 	{
+		public ICollection<Chemical> Recipes { get; set; } = new List<Chemical>();
+
 		public Day14Input Parse(string[] values)
 		{
-			throw new NotImplementedException();
+			//2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+
+			foreach (var value in values)
+			{
+				var dependencies = value.Split('=').First();
+				var dependenciesSplit = dependencies.Split(',');
+
+				var chemical = new Chemical();
+				Recipes.Add(chemical);
+
+				var chemicalStr = value.Split('>').ElementAt(1).Trim();
+				var splitChemicalStr = chemicalStr.Split(' ');
+				chemical.Name = splitChemicalStr.ElementAt(1).Trim();
+				chemical.ResultCount = int.Parse(splitChemicalStr.First().Trim());
+
+				foreach (var dependency in dependenciesSplit)
+				{
+					var dependencySplit = dependency.Trim().Split(' ');
+					chemical.Dependencies.Add(new ChemicalDependency
+											  {
+												  Count = int.Parse(dependencySplit.First().Trim()),
+												  Name = dependencySplit.ElementAt(1).Trim()
+											  });
+				}
+			}
+
+			return this;
 		}
 	}
 }
