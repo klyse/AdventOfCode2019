3ce74ef4eaeaa9e31a7cb24c3f5d5056686e6819
Unknown <Klaus.Pruenster@gmail.com>
2019-12-21 09:55:26 +0100

    prepared day 18

---
+++ Solver/Algorithms/Day18Solver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Solver.Base;
+using Solver.Model;
+
+namespace Solver.Algorithms
+{
+	public class Day18Solver : ISolver<int, Day18Input>
+	{
+		public int Star1(Day18Input input)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int Star2(Day18Input input)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Solver/Model/Day18Input.cs
@@ -0,0 +1,12 @@
+﻿using Solver.Base;
+
+namespace Solver.Model
+{
+	public class Day18Input : IInput<Day18Input>
+	{
+		public Day18Input Parse(string[] values)
+		{
+			return this;
+		}
+	}
+}
