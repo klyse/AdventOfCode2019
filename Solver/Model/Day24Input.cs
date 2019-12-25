3a679c9ec76729b9e9911f0432fe48a95d2d4a08
Unknown <Klaus.Pruenster@gmail.com>
2019-12-25 12:26:18 +0100

    day 24 input

--- Solver/Model/Day24Input.cs
+++ Solver/Model/Day24Input.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿using System.Linq;
+using NeoMatrix;
 using Solver.Base;
 
 namespace Solver.Model
 {
 	public class Day24Input : IInput<Day24Input>
 	{
+		public Matrix<bool[]> Eris { get; set; }
+
 		public Day24Input Parse(string[] values)
 		{
-			throw new NotImplementedException();
+			Eris = Matrix<bool[]>.NewMatrix(values.Length, values.First().Length, (row, column) => new[]
+																								   {
+																									   values[row][column] == '#',
+																									   false
+																								   });
+			return this;
 		}
 	}
 }
